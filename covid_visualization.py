7588f7a24284e89105caa7058da632eca9fc7f1d
N-eeraj <neeraj7rajeena@gmail.com>
2020-08-26 16:18:19 +0530

    added death plot

--- covid_visualization.py
+++ covid_visualization.py
@@ -5,11 +5,21 @@ import matplotlib.pyplot as plt
 with open('positive.csv', 'r') as pos_data_file:
 	pos_data_lines = pos_data_file.readlines()
 
-#dictionary to turn it into a dataframe
+with open('death.csv', 'r') as death_data_file:
+	death_data_lines = death_data_file.readlines()
+
+#dictionary to turn it into a positive dataframe
 pos_data_dict = {'kasaragod': np.array([]), 'kannur': np.array([]), 'wayanad': np.array([]), 'kozhikode': np.array([]),
 	'malappuram': np.array([]), 'palakkad': np.array([]), 'thrissur': np.array([]), 'ernakulam': np.array([]),
 	'idukki': np.array([]), 'kottayam': np.array([]), 'pathanamthitta': np.array([]), 'alappuzha': np.array([]),
 	'kollam': np.array([]), 'thiruvananthapuram': np.array([])}
+
+#dictionary to turn it into a death dataframe
+death_data_dict = {'kasaragod': np.array([]), 'kannur': np.array([]), 'wayanad': np.array([]), 'kozhikode': np.array([]),
+	'malappuram': np.array([]), 'palakkad': np.array([]), 'thrissur': np.array([]), 'ernakulam': np.array([]),
+	'idukki': np.array([]), 'kottayam': np.array([]), 'pathanamthitta': np.array([]), 'alappuzha': np.array([]),
+	'kollam': np.array([]), 'thiruvananthapuram': np.array([])}
+
 #date list to store dates to use as indices
 date = np.array([])
 
@@ -41,10 +51,33 @@ for pos_data_line in pos_data_lines[1:]:
 	pos_data_dict['kollam'] = np.append(pos_data_dict['kollam'], pos_data_cell[13]).astype(np.int)
 	pos_data_dict['thiruvananthapuram'] = np.append(pos_data_dict['thiruvananthapuram'], pos_data_cell[14].strip()).astype(np.int)
 
+#read  death_data to death_dictionary
+for death_data_line in death_data_lines[1:]:
+	death_data_cell = death_data_line.split(',')
+	death_data_dict['kasaragod'] = np.append(death_data_dict['kasaragod'], death_data_cell[1]).astype(np.int)
+	death_data_dict['kannur'] = np.append(death_data_dict['kannur'], death_data_cell[2]).astype(np.int)
+	death_data_dict['wayanad'] = np.append(death_data_dict['wayanad'], death_data_cell[3]).astype(np.int)
+	death_data_dict['kozhikode'] = np.append(death_data_dict['kozhikode'], death_data_cell[4]).astype(np.int)
+	death_data_dict['malappuram'] = np.append(death_data_dict['malappuram'], death_data_cell[5]).astype(np.int)
+	death_data_dict['palakkad'] = np.append(death_data_dict['palakkad'], death_data_cell[6]).astype(np.int)
+	death_data_dict['thrissur'] = np.append(death_data_dict['thrissur'], death_data_cell[7]).astype(np.int)
+	death_data_dict['ernakulam'] = np.append(death_data_dict['ernakulam'], death_data_cell[8]).astype(np.int)
+	death_data_dict['idukki'] = np.append(death_data_dict['idukki'], death_data_cell[9]).astype(np.int)
+	death_data_dict['kottayam'] = np.append(death_data_dict['kottayam'], death_data_cell[10]).astype(np.int)
+	death_data_dict['pathanamthitta'] = np.append(death_data_dict['pathanamthitta'], death_data_cell[11]).astype(np.int)
+	death_data_dict['alappuzha'] = np.append(death_data_dict['alappuzha'], death_data_cell[12]).astype(np.int)
+	death_data_dict['kollam'] = np.append(death_data_dict['kollam'], death_data_cell[13]).astype(np.int)
+	death_data_dict['thiruvananthapuram'] = np.append(death_data_dict['thiruvananthapuram'], death_data_cell[14].strip()).astype(np.int)
+
 #data_frame for positive cases
 pos_df = pd.DataFrame(pos_data_dict)
 pos_df.index = date
 #print(pos_df)
+
+#data_frame for death cases
+death_df = pd.DataFrame(death_data_dict)
+death_df.index = date
+#print(death_df)
 
 #totalling positive cases
 first = True
@@ -56,6 +89,17 @@ for district in pos_data_dict.keys():
 		pos_total += (pos_data_dict[district])
 pos_total.astype(np.int)
 
+#totalling death cases
+first = True
+for district in death_data_dict.keys():
+	if first:
+		death_total = (death_data_dict[district])
+		first = False
+	else:
+		death_total += (death_data_dict[district])
+death_total.astype(np.int)
+
+#confirming date in dataframe
 def read_date():
 	search_month = input('\nEnter Month: ').capitalize()[:3]
 	search_day = input('Enter Date: ')
@@ -65,11 +109,12 @@ def read_date():
 		return True
 	else:
 		return search_date
+
+#reading district
 def read_district():
 	search_district = input('1.Thiruvananthapuram\n2.Kollam\n3.Pathanamthitta\n4.Alappuzha\n5.Kottayam\n6.Idukki\n7.Ernakulam' +
 		'\n8.Thrissur\n9.Palakkad\n10.Malappuram\n11.Kozhikode\n12.Wayanad\n13.Kannur\n14.Kasaragod\n\n').lower()
 	district_result = None
-
 	if search_district in districts['tvm']:
 		district_result = 'thiruvananthapuram'
 	elif search_district in districts['klm']:
@@ -101,7 +146,9 @@ def read_district():
 	return district_result
 
 while True:
-	plot_selection = input('1. Date vs Cases\n2. Cases on a specific date\n3. Cases on specific date & district\n')
+	plot_selection = input('1. Positive Cases (Date vs Cases)\n2. Positive cases on a specific date\n'+
+				'3. Positive cases on specific date & district\n4. Death Cases (Date vs Cases)\n')
+
 	if plot_selection == '1':
 		plot_title = 'Positive COVID 19 Cases'
 		print('\n0.Total')
@@ -180,7 +227,6 @@ while True:
 			print('Kerala')
 			plt.plot(date, pos_total)
 			max_y = pos_total.max()
-
 		yhigh = int(max_y * 1.1) + 1
 		plt.title(plot_title)
 		plt.xlabel('Days')
@@ -215,6 +261,92 @@ while True:
 			continue
 		print('Cases on ' + date_input + ' in ' + search_district)
 		print(search_result , '\n\n')
+
+	elif plot_selection == '4':
+		plot_title = 'COVID 19 Death Cases'
+		print('\n0.Total')
+		plot_option = read_district()
+		if plot_option == 'thiruvananthapuram':
+			print('Thiruvananthapuram')
+			plt.plot(death_df['thiruvananthapuram'])
+			max_y = death_df['thiruvananthapuram'].max()
+			plot_title += ' in Thiruvananthapuram'
+		elif plot_option == 'kollam':
+			print('Kollam')
+			plt.plot(death_df['kollam'])
+			max_y = death_df['kollam'].max()
+			plot_title += ' in Kollam'
+		elif plot_option == 'pathanamthitta':
+			print('Pathanamthitta')
+			plt.plot(death_df['pathanamthitta'])
+			max_y = death_df['pathanamthitta'].max()
+			plot_title += ' in Pathhanamthitta'
+		elif plot_option == 'alappuzha':
+			print('Alappuzha')
+			plt.plot(death_df['alappuzha'])
+			max_y = death_df['alappuzha'].max()
+			plot_title += ' in Alappuzha'
+		elif plot_option == 'Kottayam':
+			print('Kottayam')
+			plt.plot(death_df['kottayam'])
+			max_y = death_df['kottayam'].max()
+			plot_title += ' in Kottayam'
+		elif plot_option == 'idukki':
+			print('Idukki')
+			plt.plot(death_df['idukki'])
+			max_y = death_df['idukki'].max()
+			plot_title += ' in Idukki'
+		elif plot_option == 'ernakulam':
+			print('Ernakulam')
+			plt.plot(death_df['ernakulam'])
+			max_y = death_df['ernakulam'].max()
+			plot_title += ' in Ernakulam'
+		elif plot_option == 'thrissur':
+			print('Thrissur')
+			plt.plot(death_df['thrissur'])
+			max_y = death_df['thrissur'].max()
+			plot_title += ' in Thrissur'
+		elif plot_option == 'palakkad':
+			print('Palakkad')
+			plt.plot(death_df['palakkad'])
+			max_y = death_df['palakkad'].max()
+			plot_title += ' in Palakkad'
+		elif plot_option == 'malappuram':
+			print('Malappuram')
+			plt.plot(death_df['malappuram'])
+			max_y = death_df['malappuram'].max()
+			plot_title += ' in Malappuram'
+		elif plot_option == 'kozhikode':
+			print('Kozhikode')
+			plt.plot(death_df['kozhikode'])
+			max_y = death_df['kozhikode'].max()
+			plot_title += ' in Kozhikode'
+		elif plot_option == 'wayanad':
+			print('Wayanad')
+			plt.plot(death_df['wayanad'])
+			max_y = death_df['wayanad'].max()
+			plot_title += ' in Wayanad'
+		elif plot_option == 'kannur':
+			print('Kannur')
+			plt.plot(death_df['kannur'])
+			max_y = death_df['kannur'].max()
+			plot_title += ' in Kannur'
+		elif plot_option == 'kasaragod':
+			print('Kasaragod')
+			plt.plot(death_df['kasaragod'])
+			max_y = death_df['kasaragod'].max()
+			plot_title += ' in Kasaragod'
+		else:
+			print('Kerala')
+			plt.plot(date, death_total)
+			max_y = death_total.max()
+		yhigh = int(max_y * 1.1) + 1
+		plt.title(plot_title)
+		plt.xlabel('Days')
+		plt.ylabel('Deaths Reported')
+		plt.ylim(0, yhigh)
+		plt.show()
+
 	else:
 		print('Exiting...')
 		exit()
