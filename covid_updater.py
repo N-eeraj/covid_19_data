45f29b700925f3ae067eae997bba78d6ddf7ac1a
N-eeraj <neeraj7rajeena@gmail.com>
2020-08-26 15:53:48 +0530

    Added death count updater

--- covid_updater.py
+++ covid_updater.py
@@ -3,20 +3,22 @@ import numpy as np
 
 districts = ['Kasaragod', 'Kannur', 'Wayanad', 'Kozhikode', 'Malappuram', 'Palakkad', 'Thrissur', 'Ernakulam', 'Idukki',
 		'Kottayam', 'Pathanamthitta', 'Alapuzha', 'Kollam', 'Thiruvananthapuram']
-date = datetime.date.today().strftime('\n%d-%b,')
-data = []
-print('Date:'+ date)
+date = datetime.date.today().strftime('%d-%b,')
+print('Date: '+ date)
 
-for district in districts :
-	count = input('Enter the positive cases in ' + district + ' :')
-	data.append(count)
+def read_data(prompt, file):
+	data = []
+	print('\n')
+	for district in districts :
+		count = input(prompt + district + ' :')
+		data.append(count)
+	data = np.array(data, dtype = int)
+	total = data.sum()
+	data = np.array(data, dtype = str)
+	print('\nTotal:'+ str(total))
+	new_line = date + (','.join(data))
+	with open(file, 'a') as data_file:
+		data_file.write(new_line)
 
-data = np.array(data, dtype = int)
-total = data.sum()
-data = np.array(data, dtype = str)
-print('\nTotal:'+ str(total))
-
-new_line = date + (','.join(data))
-
-with open('positive.csv','a') as pos_data_file:
-	pos_data_file.write(new_line)
+read_data('Enter the positive cases in ', 'positive.csv')
+read_data('Enter the death cases in ', 'death.csv')
