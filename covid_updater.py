f65c66e89ee99fa3e74cbc2930dba849dae9d4ec
N-eeraj <neeraj7rajeena@gmail.com>
2020-08-31 11:42:41 +0530

    added comments

--- covid_updater.py
+++ covid_updater.py
@@ -1,9 +1,9 @@
-import datetime
 import numpy as np
 
 districts = ['Kasaragod', 'Kannur', 'Wayanad', 'Kozhikode', 'Malappuram', 'Palakkad', 'Thrissur', 'Ernakulam', 'Idukki',
 		'Kottayam', 'Pathanamthitta', 'Alapuzha', 'Kollam', 'Thiruvananthapuram']
 
+#reading date
 day = input('Enter Day : ')
 month = input('Enter Month : ').capitalize()[:3]
 date = month + '-' + day
@@ -14,15 +14,16 @@ def read_data(prompt, file):
 	print('\n')
 	for district in districts :
 		count = input(prompt + district + ' :')
-		data.append(count)
-	data = np.array(data, dtype = int)
-	total = data.sum()
+		data.append(count) #making a list of new entries
+	data = np.array(data, dtype = int) #converting list to numpy array
+	total = data.sum() #totalling the count
 	data = np.array(data, dtype = str)
 	print('\nTotal:'+ str(total))
-	new_line = date + (','.join(data))
+	new_line = date + (','.join(data)) #transforming array to a comma sepsrated line as new line
 	with open(file, 'a') as data_file:
-		data_file.write('\n' + new_line)
+		data_file.write('\n' + new_line) #appending the new line to the file
 
+#adding data to the respective files
 read_data('Enter the positive cases in ', 'positive.csv')
 read_data('Enter the death cases in ', 'death.csv')
 read_data('Enter the recovery cases in ', 'recovery.csv')
