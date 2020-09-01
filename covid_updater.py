8c2b4853c99cec0c91ee788f35bf76b55ee5cdaf
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-01 21:19:16 +0530

    fixed error in new_line joining

--- covid_updater.py
+++ covid_updater.py
@@ -19,7 +19,7 @@ def read_data(prompt, file):
 	total = data.sum() #totalling the count
 	data = np.array(data, dtype = str)
 	print('\nTotal:'+ str(total))
-	new_line = date + (','.join(data)) #transforming array to a comma sepsrated line as new line
+	new_line = date + ',' + (','.join(data)) #transforming array to a comma sepsrated line as new line
 	with open(file, 'a') as data_file:
 		data_file.write('\n' + new_line) #appending the new line to the file
 
