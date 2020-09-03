40be79d95ee11ae75610ee8badbefe1880f415af
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-03 19:54:51 +0530

    reading all data of a district together

--- covid_updater.py
+++ covid_updater.py
@@ -9,21 +9,34 @@ month = input('Enter Month : ')[:3].capitalize()
 date = day + '-' + month
 print('Date: '+ date)
 
-def read_data(prompt, file):
-	data = []
-	print('\n')
-	for district in districts :
-		count = input(prompt + district + ' :')
-		data.append(count) #making a list of new entries
-	data = np.array(data, dtype = int) #converting list to numpy array
-	total = data.sum() #totalling the count
-	data = np.array(data, dtype = str)
-	print('\nTotal:'+ str(total))
-	new_line = date + ',' + (','.join(data)) #transforming array to a comma sepsrated line as new line
-	with open(file, 'a') as data_file:
-		data_file.write('\n' + new_line) #appending the new line to the file
+pos_list = [date]
+recovery_list = [date]
+death_list = [date]
+
+for district in districts:
+	pos_list.append(input('Enter the positive cases in ' + district))
+	recovery_list.append(input('Enter the recovered cases in ' + district))
+	death_list.append(input('Enter the death cases in ' + district))
+
+print(pos_list)
+print(recovery_list)
+print(death_list)
+
+#def read_data(prompt, file):
+#	data = []
+#	print('\n')
+#	for district in districts :
+#		count = input(prompt + district + ' :')
+#		data.append(count) #making a list of new entries
+#	data = np.array(data, dtype = int) #converting list to numpy array
+#	total = data.sum() #totalling the count
+#	data = np.array(data, dtype = str)
+#	print('\nTotal:'+ str(total))
+#	new_line = date + ',' + (','.join(data)) #transforming array to a comma sepsrated line as new line
+#	with open(file, 'a') as data_file:
+#		data_file.write('\n' + new_line) #appending the new line to the file
 
 #adding data to the respective files
-read_data('Enter the positive cases in ', 'positive.csv')
-read_data('Enter the death cases in ', 'death.csv')
-read_data('Enter the recovery cases in ', 'recovery.csv')
+#read_data('Enter the positive cases in ', 'positive.csv')
+#read_data('Enter the death cases in ', 'death.csv')
+#read_data('Enter the recovery cases in ', 'recovery.csv')
