893ba3c0738a6cc9d00cb3f47f67357e540a5711
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-03 21:10:22 +0530

    added totaling

--- covid_updater.py
+++ covid_updater.py
@@ -10,37 +10,18 @@ date = day + '-' + month
 print('Date: '+ date)
 
 pos_list = [date]
-recovery_list = [date]
-death_list = [date]
+rec_list = [date]
+dead_list = [date]
 
-def fn_save(file, data_list):
-	with open(file, 'a') as data_file:
-		data_file.write('\n' + ','.join(data_list))
+def fn_save(file, data_list): #function to update a file
+	with open(file, 'a') as data_file: #opening file
+		data_file.write('\n' + ','.join(data_list)) #updating file
 
 for district in districts:
-	pos_list.append(input('\nEnter the positive cases in ' + district + ' : '))
-	recovery_list.append(input('Enter the recovered cases in ' + district + ' : '))
-	death_list.append(input('Enter the death cases in ' + district + ' : '))
+	pos_list.append(input('\nEnter the positive cases in ' + district + ' : ')) #reading positive cases
+	rec_list.append(input('Enter the recovered cases in ' + district + ' : ')) #reading recovery cases
+	dead_list.append(input('Enter the death cases in ' + district + ' : ')) #reading death cases
 
-for files in [('positive.csv', pos_list), ('recovery.csv', recovery_list), ('death.csv', death_list)]:
+for files in [('positive.csv', pos_list), ('recovery.csv', rec_list), ('death.csv', dead_list)]:
 	print('\n' + files[0][:-4].capitalize(), 'Cases:' , np.array(files[1][1:], dtype = int).sum())
 	fn_save(files[0], files[1])
-
-#def read_data(prompt, file):
-#	data = []
-#	print('\n')
-#	for district in districts :
-#		count = input(prompt + district + ' :')
-#		data.append(count) #making a list of new entries
-#	data = np.array(data, dtype = int) #converting list to numpy array
-#	total = data.sum() #totalling the count
-#	data = np.array(data, dtype = str)
-#	print('\nTotal:'+ str(total))
-#	new_line = date + ',' + (','.join(data)) #transforming array to a comma sepsrated line as new line
-#	with open(file, 'a') as data_file:
-#		data_file.write('\n' + new_line) #appending the new line to the file
-
-#adding data to the respective files
-#read_data('Enter the positive cases in ', 'positive.csv')
-#read_data('Enter the death cases in ', 'death.csv')
-#read_data('Enter the recovery cases in ', 'recovery.csv')
