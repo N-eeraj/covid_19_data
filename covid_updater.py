d541d0700ea72d8e6932ca6af86c2d53e478aed9
N-eeraj <neeraj7rajeena@gmail.com>
2020-09-07 21:52:24 +0530

    added validation of total cases, updates files that is validated

--- covid_updater.py
+++ covid_updater.py
@@ -9,19 +9,29 @@ month = input('Enter Month : ')[:3].capitalize()
 date = day + '-' + month
 print('Date: '+ date)
 
-pos_list = [date]
-rec_list = [date]
-dead_list = [date]
-
 def fn_save(file, data_list): #function to update a file
 	with open(file, 'a') as data_file: #opening file
 		data_file.write('\n' + ','.join(data_list)) #updating file
 
-for district in districts:
-	pos_list.append(input('\nEnter the positive cases in ' + district + ' : ')) #reading positive cases
-	rec_list.append(input('Enter the recovered cases in ' + district + ' : ')) #reading recovery cases
-	dead_list.append(input('Enter the death cases in ' + district + ' : ')) #reading death cases
+while True:
 
-for files in [('positive.csv', pos_list), ('recovery.csv', rec_list), ('death.csv', dead_list)]:
-	print('\n' + files[0][:-4].capitalize(), 'Cases:' , np.array(files[1][1:], dtype = int).sum()) #calculate sums
-	fn_save(files[0], files[1]) #updating a file
+	exit = True
+	pos_list = [date]
+	rec_list = [date]
+	dead_list = [date]
+
+	for district in districts:
+		pos_list.append(input('\nEnter the positive cases in ' + district + ' : ')) #reading positive cases
+		rec_list.append(input('Enter the recovered cases in ' + district + ' : ')) #reading recovery cases
+		dead_list.append(input('Enter the death cases in ' + district + ' : ')) #reading death cases
+
+	for files in [('positive.csv', pos_list), ('recovery.csv', rec_list), ('death.csv', dead_list)]:
+		print('\n' + files[0][:-4].capitalize(), 'Cases:' , np.array(files[1][1:], dtype = int).sum()) #calculate sums
+		if input('Confirm Total ' +  files[0][:-4] + ' Cases (y/n)').lower() != 'y':
+			print('Check the numbers & try again')
+			exit = False
+			break
+		fn_save(files[0], files[1]) #updating a file
+
+	if exit == True:
+		exit() #exit on updating
